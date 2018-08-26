3694a4d38c40e2c5a00f667c6bf7108ac485537d
Kelvin Hsu <chechunhsu@gmail.com>
2018-08-27 00:08:52 +0800

    build: move images out of firebase

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -27,10 +27,10 @@ export default class DpickDocument extends Document {
         <meta property='og:title' content={locale('title')}/>
         <meta property='og:type' content='Website'/>
         <meta property='og:url' content='https://who-is-spy.firebaseapp.com/'/>
-        <meta property='og:image:secure_url' content='https://who-is-spy.firebaseapp.com/img/profile.jpg'/>
+        <meta property='og:image:secure_url' content='https://pli.io/kphO7.jpg'/>
         <meta property='og:image:width' content='378'/>
         <meta property='og:image:height' content='378'/>
-        <meta property='og:image' content='https://who-is-spy.firebaseapp.com/img/profile.jpg'/>
+        <meta property='og:image' content='https://pli.io/kphO7.jpg'/>
         <meta property='og:site_name' content={locale('title')}/>
         <meta property='og:description' content={locale('description')}/>
 
@@ -42,12 +42,12 @@ export default class DpickDocument extends Document {
         <link rel='stylesheet' type='text/css' href='/normalize.css'/>
 
         <link rel='shortcut icon' href='/img/favicon.ico' type='image/x-icon'/>
-        <link rel='icon' type='image/png' href='/img/favicon-32x32.png' sizes='32x32'/>
-        <link rel='icon' type='image/png' href='/img/favicon-48x48.png' sizes='48x48'/>
-        <link rel='icon' type='image/png' href='/img/favicon-96x96.png' sizes='96x96'/>
-        <link rel='icon' type='image/png' href='/img/favicon-144x144.png' sizes='144x144'/>
-        <link rel='icon' type='image/png' href='/img/favicon-512x512.png' sizes='512x512'/>
-        <link rel='apple-touch-icon' sizes='180x180' href='/img/apple-touch-icon.png'/>
+        <link rel='icon' type='image/png' href='https://pli.io/kpfKu.png' sizes='32x32'/>
+        <link rel='icon' type='image/png' href='https://pli.io/kpYO3.png' sizes='48x48'/>
+        <link rel='icon' type='image/png' href='https://pli.io/kpsB9.png' sizes='96x96'/>
+        <link rel='icon' type='image/png' href='https://pli.io/kpdCw.png' sizes='144x144'/>
+        <link rel='icon' type='image/png' href='https://pli.io/kpFEy.png' sizes='512x512'/>
+        <link rel='apple-touch-icon' sizes='180x180' href='https://pli.io/kpKiQ.png'/>
 
         {this.props.styleTags}
       </Head>
